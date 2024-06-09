ab079908d551f2223cfc6fb3bf3e874e8067f3a8
privilledge <privieroyalty@gmail.com>
2024-06-09 22:44:49 +0200

    cart and images

--- src/main/java/com/privilledge/backend_ecommerce/config/SecurityConfig.java
+++ src/main/java/com/privilledge/backend_ecommerce/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 //package com.privilledge.backend_ecommerce.config;
 //
 //import org.springframework.context.annotation.Configuration;
+//
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //
 //@Configuration
--- src/main/java/com/privilledge/backend_ecommerce/model/User.java
+++ src/main/java/com/privilledge/backend_ecommerce/model/User.java
@@ -1,11 +1,9 @@
 package com.privilledge.backend_ecommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
